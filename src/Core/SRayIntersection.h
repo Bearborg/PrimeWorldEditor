712395b2410e257d33aad46d6922b487a19cef4a
Lioncache <mai.iam2048@gmail.com>
2025-12-14 18:13:46 -0500

    SRayIntersection: Pass vector by const reference
    Just avoids an unnecessary copy.

--- src/Core/SRayIntersection.h
+++ src/Core/SRayIntersection.h
@@ -1,8 +1,8 @@
 #ifndef SRAYINTERSECTION
 #define SRAYINTERSECTION
 
-#include <Common/BasicTypes.h>
 #include <Common/Math/CVector3f.h>
+#include <cstdint>
 
 class CSceneNode;
 
@@ -12,10 +12,10 @@ struct SRayIntersection
     float Distance = 0.0f;
     CVector3f HitPoint{CVector3f::Zero()};
     CSceneNode *pNode = nullptr;
-    uint ComponentIndex = UINT32_MAX;
+    uint32_t ComponentIndex = UINT32_MAX;
 
     SRayIntersection() = default;
-    SRayIntersection(bool _Hit, float _Distance, CVector3f _HitPoint, CSceneNode *_pNode, uint _ComponentIndex)
+    SRayIntersection(bool _Hit, float _Distance, const CVector3f& _HitPoint, CSceneNode *_pNode, uint32_t _ComponentIndex)
         : Hit(_Hit), Distance(_Distance), HitPoint(_HitPoint), pNode(_pNode), ComponentIndex(_ComponentIndex) {}
 };
 
